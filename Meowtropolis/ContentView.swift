776ce272a8dcdf56e9d443f2a4aa70eed6193d0a
mayer-doa-coder <hasan2107004@stud.kuet.ac.bd>
2026-03-31 02:59:14 +0600

    feat: initialize project with swiftui template

---
+++ Meowtropolis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meowtropolis
+//
+//  Created by MD.Ashraful Islam  on 31/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meowtropolis/MeowtropolisApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeowtropolisApp.swift
+//  Meowtropolis
+//
+//  Created by MD.Ashraful Islam  on 31/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MeowtropolisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
